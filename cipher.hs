3322ef971147d6cba6b48b43ec701ddaa4b0f55b
tomasz-lutoborski <tomek218@hotmail.com>
2023-03-21 21:00:52 +0100

    Done types unit

--- cipher.hs
+++ cipher.hs
@@ -109,3 +109,22 @@ applyOTP :: String -> String -> String
 applyOTP pad plaintext = map bitsToChar bitList
   where
     bitList = applyOTP' pad plaintext
+
+class Cipher a where
+  encode :: a -> String -> String
+  decode :: a -> String -> String
+
+data Rot = Rot
+
+instance Cipher Rot where
+  encode Rot text = rotEncoder text
+  decode Rot text = rotDecoder text
+
+data OneTimePad = OTP String
+
+instance Cipher OneTimePad where
+  encode (OTP pad) text = applyOTP pad text
+  decode (OTP pad) text = applyOTP pad text
+
+myOTP :: OneTimePad
+myOTP = OTP (cycle [minBound .. maxBound])
