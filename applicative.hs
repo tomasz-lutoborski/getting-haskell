237a7fccc3a47579706c3dbb5114d3027e9907f5
tomasz-lutoborski <tomek218@hotmail.com>
2023-03-25 18:42:32 +0100

    done part of unit 5

---
+++ applicative.hs
@@ -0,0 +1,107 @@
+import Data.Map qualified as Map
+
+type LatLong = (Double, Double)
+
+locationDB :: Map.Map String LatLong
+locationDB =
+  Map.fromList
+    [ ("Arkham", (42.6054, -70.7829)),
+      ("Innsmouth", (42.8250, -70.8150)),
+      ("Carcosa", (29.9714, -90.7694)),
+      ("New York", (40.7776, -73.9691))
+    ]
+
+toRadians :: Double -> Double
+toRadians degrees = degrees * pi / 180
+
+latLongToRads :: LatLong -> (Double, Double)
+latLongToRads (lat, long) = (rlat, rlong)
+  where
+    rlat = toRadians lat
+    rlong = toRadians long
+
+haversine :: LatLong -> LatLong -> Double
+haversine coords1 coords2 = earthRadius * c
+  where
+    (rlat1, rlong1) = latLongToRads coords1
+    (rlat2, rlong2) = latLongToRads coords2
+    dlat = rlat2 - rlat1
+    dlong = rlong2 - rlong1
+    a = (sin (dlat / 2)) ^ 2 + cos rlat1 * cos rlat2 * (sin (dlong / 2)) ^ 2
+    c = 2 * atan2 (sqrt a) (sqrt (1 - a))
+    earthRadius = 3961.0
+
+haversineMaybe :: Maybe LatLong -> Maybe LatLong -> Maybe Double
+haversineMaybe Nothing _ = Nothing
+haversineMaybe _ Nothing = Nothing
+haversineMaybe (Just val1) (Just val2) = Just (haversine val1 val2)
+
+haversineIO :: IO LatLong -> IO LatLong -> IO Double
+haversineIO ioVal1 ioVal2 = do
+  val1 <- ioVal1
+  val2 <- ioVal2
+  return (haversine val1 val2)
+
+haversineIO' :: IO LatLong -> IO LatLong -> IO Double
+haversineIO' ioVal1 ioVal2 = haversine <$> ioVal1 <*> ioVal2
+
+printDistance :: Maybe Double -> IO ()
+printDistance Nothing = putStrLn "Error, invalid city entered"
+printDistance (Just distance) = putStrLn (show distance ++ " miles")
+
+-- main :: IO ()
+-- main = do
+--   putStrLn "Enter the starting city name:"
+--   startingInput <- getLine
+--   let startingCity = Map.lookup startingInput locationDB
+--   putStrLn "Enter the destination city name:"
+--   destInput <- getLine
+--   let destCity = Map.lookup destInput locationDB
+--   let distance = haversine <$> startingCity <*> destCity
+--   printDistance distance
+
+minOfThree :: Ord a => a -> a -> a -> a
+minOfThree x y z = min x (min y z)
+
+readInt :: IO Int
+readInt = read <$> getLine
+
+minOfInts :: IO Int
+minOfInts = minOfThree <$> readInt <*> readInt <*> readInt
+
+main :: IO ()
+main = do
+  putStrLn "Enter three integers, and I'll tell you the smallest one:"
+  min <- minOfInts
+  putStrLn ("The smallest one is " ++ show min)
+
+primesToN :: Integer -> [Integer]
+primesToN n = filter isNotComposite twoThroughN
+  where
+    twoThroughN = [2 .. n]
+    composite = pure (*) <*> twoThroughN <*> twoThroughN
+    isNotComposite = not . (`elem` composite)
+
+allFmap :: Applicative f => (a -> b) -> f a -> f b
+allFmap f x = pure f <*> x
+
+beerStart :: [Int]
+beerStart = [6, 12]
+
+beerAfterYesterday :: [Int]
+beerAfterYesterday = (\x -> x - 4) <$> beerStart
+
+guests :: [Int]
+guests = [2, 3]
+
+allPeople :: [Int]
+allPeople = (\x -> x + 2) <$> guests
+
+beerPerPerson :: [Int]
+beerPerPerson = [3, 4]
+
+totalBeerNeeded :: [Int]
+totalBeerNeeded = (*) <$> allPeople <*> beerPerPerson
+
+beerToPurchase :: [Int]
+beerToPurchase = pure (-) <*> totalBeerNeeded <*> beerAfterYesterday
